17b07cb0de4531b0f77b3c7e251e4c38468a63a7
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2024-07-19 16:16:00 -0300

    added some docstring to LicensePlateModel class

--- models/plate.py
+++ models/plate.py
@@ -1,3 +1,7 @@
+"""Model class to solve the License Plate classification problem."""
+
+__author__ = "Lucas C. Araujo"
+
 import os
 from torch import nn, save, load
 from torchvision.models import MobileNetV3, MobileNet_V3_Small_Weights, WeightsEnum
@@ -7,16 +11,45 @@ from utils.trainer import Trainer
 weightsDir: str = Trainer.weightsDir
 
 class LicensePlateModel(MobileNetV3):
-    def __init__(self, 
-                 numClasses:int = 36, 
-                 customPreTrained: bool = False,
-                 preTrainedPath: str = ""):
+    def __init__(
+            self, 
+            numClasses:int = 36, 
+            customPreTrained: bool = False,
+            preTrainedPath: str = ""):
+        """
+        License Plate Model class, based on the MobileNetV3 architecture.
+        
+        Initialize the LicensePlateModel class with the given number of classes.
+        The model uses the 'mobilenet_v3_small' configuration. 
+        The model is pre-trained with the IMAGENET dataset.
+        
+        Optionally, the user can load a pre-trained model from the 'weightsDir' path or
+        from a given path.
+        
+        if 'customPreTrained' is True and 'preTrainedPath' is empty, it searches for a pre-trained
+        and shows a list of models to the user. The user can choose one to load.
+         
+        Args:
+            numClasses (int, optional): Number of classes for the model. Defaults to 36.
+            customPreTrained (bool, optional): Load a pre-trained model. Defaults to False.
+            preTrainedPath (str, optional): Path to the pre-trained model. Defaults to "".
+        """
+
+        # Load the MobileNetV3 model with the 'mobilenet_v3_small' configuration
+        # 1. Get mobilenet_v3_small configuration
         inverted_residual_setting, last_channel = mobilenetv3._mobilenet_v3_conf("mobilenet_v3_small")
+        # 2. Get the IMAGENET weights
         weights: WeightsEnum = MobileNet_V3_Small_Weights.verify("DEFAULT")
+        # 3. Initialize the model with the configuration and the default number of classes
         super(LicensePlateModel, self).__init__(
                 inverted_residual_setting, last_channel, num_classes=len(weights.meta["categories"]))
+        # 4. Load the IMAGENET weights into the model
         self.load_state_dict(weights.get_state_dict(check_hash=True))
+
+        # After loaded the model, change the last layer to match the number of classes
         self.classifier[3] = nn.Linear(self.classifier[3].in_features, numClasses)
+
+        # Load a pre-trained model if the user wants
         if customPreTrained:
             try:
                 self.load(preTrainedPath)
