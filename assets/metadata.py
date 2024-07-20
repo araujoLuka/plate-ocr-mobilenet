5b2fe0267d9411d3531ed7882a569ed6e8b385e3
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2024-07-19 21:58:28 -0300

    added metadata with DIGITS_CATEGORIES

---
+++ assets/metadata.py
@@ -0,0 +1,5 @@
+"""This module contains metadata for the project."""
+
+# Define the categories for the digits.
+# - A list with all numbers and uppercase letters.
+DIGITS_CATEGORIES = [str(x) for x in range(10)] + [chr(x) for x in range(65, 91)]
